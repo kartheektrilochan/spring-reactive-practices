e17110879a71ed5041e28c39cf1b25b08f502b40
trilochankartheek <kartheektrilochan@gmail.com>
2024-06-21 17:23:19 -0400

    Initia Project Setup

---
+++ src/main/java/com/kkt/learning/spring_reactive_practices/SpringReactivePracticesApplication.java
@@ -0,0 +1,13 @@
+package com.kkt.learning.spring_reactive_practices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactivePracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactivePracticesApplication.class, args);
+	}
+
+}
